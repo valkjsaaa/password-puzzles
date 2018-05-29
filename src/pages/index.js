27201bc12440ea59bbc0e3110c415ee99740fbc8
valkjsaaa <jackieyang51@gmail.com>
2018-05-28 22:00:06 -0700

    Created a dummy index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import Link from 'gatsby-link'
+import 'semantic-ui-css/semantic.min.css';
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => (
+    <div className="ui form">
+        <div className="field">
+            <label>Password</label>
+            <div className="ui fluid input">
+                <input type="text" placeholder="Enter your password here"/>
+            </div>
+        </div>
+        <div className="field">
+            <button className="ui button" role="button">Submit</button>
+        </div>
+        <Link to="/pm/">Next</Link>
+    </div>
+)
+
+export default IndexPage
