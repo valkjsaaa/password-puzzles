fb9e7e04954f6b85197e378d6fb413252f21a8e0
valkjsaaa <jackieyang51@gmail.com>
2018-05-31 12:45:37 -0700

    added script to deploy to GitHub

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        title: `Password Puzzles`,
+        siteUrl: `https://valkjsaaa.github.com`,
+        description: `Password Puzzles: a interactive walk through about passwords`,
+    },
+    pathPrefix: `/password-puzzles`
+};
